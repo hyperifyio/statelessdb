2169fd08fc9ef5ac20f827dc58ac32248118eeea
Jaakko Heusala <jhh@hg.fi>
2024-10-20 21:08:37 +0300

    Added active pool monitor https://github.com/hyperifyio/statelessdb/issues/29

--- pkg/workers/pool.go
+++ pkg/workers/pool.go
@@ -6,8 +6,10 @@ package workers
 import (
 	"context"
 	"errors"
+	"fmt"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 var (
@@ -21,6 +23,10 @@ const (
 	StateStopped      uint32 = iota // Initial state, workers not started
 	StateRunning                    // Workers are running
 	StateShuttingDown               // Workers are shutting down
+)
+
+const (
+	StatusUpdateInterval = 5 // How often to print status from the pool if it's working
 )
 
 // Pool defines the interface for a worker pool.
@@ -50,12 +56,15 @@ type Pool[T any] interface {
 // WorkerPool is an implementation of the Pool interface.
 // It manages a pool of worker goroutines to process jobs of type T.
 type WorkerPool[T any] struct {
-	jobs       chan T          // jobs is a channel for sending jobs to workers.
-	wg         sync.WaitGroup  // wg is WaitGroup to wait for all workers to finish.
-	ctx        context.Context // ctx is context for cancellation.
-	state      uint32          // state is atomic variable representing the pool's state.
-	bufferSize int             // bufferSize is the buffer size for jobs channel
-	workerFunc func(T)         // workerFunc is a function which processes a job from the pool
+	jobs               chan T          // jobs is a channel for sending jobs to workers.
+	wg                 sync.WaitGroup  // wg is WaitGroup to wait for all workers to finish.
+	ctx                context.Context // ctx is context for cancellation.
+	state              uint32          // state is atomic variable representing the pool's state.
+	bufferSize         int             // bufferSize is the buffer size for jobs channel
+	workerFunc         func(T)         // workerFunc is a function which processes a job from the pool
+	publishedJobsCount uint64          // publishedJobsCount is the amount of jobs this pool has received
+	startedJobsCount   uint64          // startedJobsCount is the amount of jobs the pool has started
+	finishedJobsCount  uint64          // finishedJobsCount is the amount of jobs the pool has processed
 }
 
 var _ Pool[int] = &WorkerPool[int]{} // Implements Pool[int]
@@ -66,9 +75,31 @@ func NewPool[T any](
 ) *WorkerPool[T] {
 	log.Debugf("NewPool: Creating a worker pool with buffer %d", bufferSize)
 	return &WorkerPool[T]{
-		ctx:        ctx,
-		bufferSize: bufferSize,
+		ctx:                ctx,
+		bufferSize:         bufferSize,
+		publishedJobsCount: 0,
+		startedJobsCount:   0,
+		finishedJobsCount:  0,
 	}
+}
+
+func (m *WorkerPool[T]) String() string {
+	return fmt.Sprintf("Pool(%d/%d/%d)", m.PublishedJobs(), m.StartedJobs(), m.FinishedJobs())
+}
+
+// PublishedJobs returns the amount of jobs received to this pool
+func (m *WorkerPool[T]) PublishedJobs() uint64 {
+	return atomic.LoadUint64(&m.publishedJobsCount)
+}
+
+// StartedJobs returns the amount of jobs started by a worker
+func (m *WorkerPool[T]) StartedJobs() uint64 {
+	return atomic.LoadUint64(&m.startedJobsCount)
+}
+
+// FinishedJobs returns the amount of jobs finished by the pool
+func (m *WorkerPool[T]) FinishedJobs() uint64 {
+	return atomic.LoadUint64(&m.finishedJobsCount)
 }
 
 // Start starts some workers to process jobs
@@ -96,6 +127,44 @@ func (m *WorkerPool[T]) Start(workers int, f func(T)) error {
 			log.Warnf("Start: Warning! Pool stop failed: %v", err)
 		}
 	}()
+
+	go func() {
+		prevPublishedCount := m.PublishedJobs()
+		prevStartedCount := m.StartedJobs()
+		prevFinishedCount := m.FinishedJobs()
+
+		ticker := time.NewTicker(StatusUpdateInterval * time.Second)
+		defer ticker.Stop()
+
+		for {
+			currentPublishedCount := m.PublishedJobs()
+			currentStartedCount := m.StartedJobs()
+			currentFinishedCount := m.FinishedJobs()
+
+			diffPublishedCount := currentPublishedCount - prevPublishedCount
+			diffStartedCount := currentStartedCount - prevStartedCount
+			diffFinishedCount := currentFinishedCount - prevFinishedCount
+
+			if diffPublishedCount != 0 || diffStartedCount != 0 || diffFinishedCount != 0 {
+				log.Infof("Pool active: Published=%d, Started=%d, Finished=%s",
+					diffPublishedCount, diffStartedCount, diffFinishedCount)
+			} else {
+				log.Debugf("Pool passive: Published=%d, Started=%d, Finished=%s",
+					diffPublishedCount, diffStartedCount, diffFinishedCount)
+			}
+
+			prevPublishedCount = m.PublishedJobs()
+			prevStartedCount = m.StartedJobs()
+			prevFinishedCount = m.FinishedJobs()
+
+			select {
+			case <-m.ctx.Done():
+				return
+			case <-ticker.C:
+			}
+		}
+	}()
+
 	return nil
 }
 
@@ -131,6 +200,7 @@ func (m *WorkerPool[T]) Publish(job T) error {
 	}
 	select {
 	case m.jobs <- job:
+		atomic.AddUint64(&m.publishedJobsCount, 1)
 		log.Debugf("Publish: Published a job")
 		return nil
 	case <-m.ctx.Done():
@@ -147,6 +217,7 @@ func (m *WorkerPool[T]) TryPublish(job T) (bool, error) {
 	}
 	select {
 	case m.jobs <- job:
+		atomic.AddUint64(&m.publishedJobsCount, 1)
 		log.Debugf("TryPublish: Published a job")
 		return true, nil
 	default:
@@ -166,7 +237,9 @@ func (m *WorkerPool[T]) TryStealWork() (bool, error) {
 			return false, ErrPoolClosed
 		}
 		log.Debugf("StealWork: Stole and started working on a job.")
+		atomic.AddUint64(&m.startedJobsCount, 1)
 		m.workerFunc(job)
+		atomic.AddUint64(&m.finishedJobsCount, 1)
 		log.Debugf("StealWork: Stole and processed a job.")
 		return true, nil
 	default:
@@ -189,7 +262,9 @@ func (m *WorkerPool[T]) worker() {
 				return
 			}
 			log.Debugf("Worker: Started working on a job.")
+			atomic.AddUint64(&m.startedJobsCount, 1)
 			m.workerFunc(job)
+			atomic.AddUint64(&m.finishedJobsCount, 1)
 			log.Debugf("Worker: Processed a job.")
 		case <-m.ctx.Done():
 			log.Debugf("Worker: Shutting down. Worker pool context closed.")
